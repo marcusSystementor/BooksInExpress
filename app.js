fdb16ca22f7582fc112b3cd7a085bdba4cfc4d3b
Marcus B <marcus.brederfalt@systementor.se>
2024-01-12 11:48:41 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const http = require("http");
+const fs = require("fs/promises");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+const booksPath = path.join(__dirname, "books.json");
+
+const readBooks = async () => {
+
+    try {
+        const data = await fs.readFile(booksPath);
+        return JSON.parse(data);
+    } catch(error) {
+        return [];
+    }
+};
+
+const writeBooks = async (data) => {
+
+    try {
+        await fs.writeFile(booksPath, JSON.stringify(data, null, 2));
+        console.log("Allt gick bra");
+    } catch(error) {
+        console.log("Fel", error);
+    }
+}
+
+app.post("/books", async(req, res) => {
+
+    const newBook = {id: Date.now(), name: req.body.name};
+    const books = await readBooks();
+    books.push(newBook);
+    await writeBooks(books);
+    res.status(201).json(newBook);
+});
+
+app.get("/books", async(req, res) => {
+    const books = await readBooks();
+    res.status(200).json(books);
+})
+
+
+const server = http.createServer(app);
+server.listen(3000);
